71bec82554f4f457c3b44df331675f2781ffe06b
akke-t <akke.toeter@gmail.com>
2018-09-22 21:41:24 +0200

    Found a Batchfile that prints the output of a cmd command.

---
+++ BashAndShell/Example scripts/CallingBatch.bat
@@ -0,0 +1,9 @@
+::If you want the output that SomeBatFile.bat generates, so not a copy of the commands that SomeBatFile.bat executes, but the actual response on those executions, then this is your answer! Don't put this code in SomeBatFile.bat but create a second separate CallSomeBatFile.bat and put this code in there. Then Execute CallSomeBatFile.bat, and it works like a charm!
+
+
+::Normally echo >> log.txt only stores the commands, and not the output of what that command returns.
+::This batch file 1. calls a different batch file 2., and prints the output  of 2. in file log2.txt 
+::(which is located in the same folder as file1).
+
+@echo off
+call SomeBatchFile.BAT >log.txt 2>&1
