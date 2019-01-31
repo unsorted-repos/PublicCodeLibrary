612feb04dc1da61eb3e2b7b5273d814368d71de7
akke-t <akke.toeter@gmail.com>
2019-02-01 00:52:11 +0100

    Removed excess tabs in property fields of object Task.

--- AutomationAndSystems/Taskwarrior/customSortServerV4/src/customSortServerV4/Task.java
+++ AutomationAndSystems/Taskwarrior/customSortServerV4/src/customSortServerV4/Task.java
@@ -4,31 +4,31 @@ import java.util.ArrayList;
 
 public class Task {
 
-	String	depends	=null;
-	String	description=null;	
-	String	due	=null; //date
-	String	end	=null; //date
-	String	entry	=null; //date
-	String	estimate=null;	
-	double	id=-1;
-	double	imask=-1;	
-	String	mask=null;	
-	String	modified	=null; //date
-	String	parent=null;	
-	String	priority=null;	
-	String	project=null;	
-	String	recur=null;	
-	String	scheduled	=null; //date
-	String	start	=null; //date
-	String	status=null;	
-	String	tags=null;	
-	double	tracnumber=-1;	
-	String	tracsummary=null;	
-	String	tracurl=null;	
-	String	until	=null; //date
-	double	urgency	=-1;
-	String	uuid=null;	
-	String	wait	=null; //date
+	 String depends =null;
+	 String description=null; 
+	 String due =null; //date
+	 String end =null; //date
+	 String entry =null; //date
+	 String estimate=null; 
+	 double id=-1;
+	 double imask=-1; 
+	 String mask=null; 
+	 String modified =null; //date
+	 String parent=null; 
+	 String priority=null; 
+	 String project=null; 
+	 String recur=null; 
+	 String scheduled =null; //date
+	 String start =null; //date
+	 String status=null; 
+	 String tags=null; 
+	 double tracnumber=-1; 
+	 String tracsummary=null; 
+	 String tracurl=null; 
+	 String until =null; //date
+	 double urgency =-1;
+	 String uuid=null; 
+	 String wait =null; //date
 	
 	//User defined attributes (UDA's)
 	int customSort =-1;
