113a97aecc925297dc15af59bb6eacc334dd7fda
akke-t <akke.toeter@gmail.com>
2019-02-01 00:13:33 +0100

    Wrote first test for object Task.

--- AutomationAndSystems/Taskwarrior/customSortServerV4/src/customSortServerV4/Task.java
+++ AutomationAndSystems/Taskwarrior/customSortServerV4/src/customSortServerV4/Task.java
@@ -44,4 +44,230 @@ public class Task {
 	public void Task() {
 
 	}
+
+	public String getDepends() {
+		return depends;
+	}
+
+	public void setDepends(String depends) {
+		this.depends = depends;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDue() {
+		return due;
+	}
+
+	public void setDue(String due) {
+		this.due = due;
+	}
+
+	public String getEnd() {
+		return end;
+	}
+
+	public void setEnd(String end) {
+		this.end = end;
+	}
+
+	public String getEntry() {
+		return entry;
+	}
+
+	public void setEntry(String entry) {
+		this.entry = entry;
+	}
+
+	public String getEstimate() {
+		return estimate;
+	}
+
+	public void setEstimate(String estimate) {
+		this.estimate = estimate;
+	}
+
+	public double getId() {
+		return id;
+	}
+
+	public void setId(double id) {
+		this.id = id;
+	}
+
+	public double getImask() {
+		return imask;
+	}
+
+	public void setImask(double imask) {
+		this.imask = imask;
+	}
+
+	public String getMask() {
+		return mask;
+	}
+
+	public void setMask(String mask) {
+		this.mask = mask;
+	}
+
+	public String getModified() {
+		return modified;
+	}
+
+	public void setModified(String modified) {
+		this.modified = modified;
+	}
+
+	public String getParent() {
+		return parent;
+	}
+
+	public void setParent(String parent) {
+		this.parent = parent;
+	}
+
+	public String getPriority() {
+		return priority;
+	}
+
+	public void setPriority(String priority) {
+		this.priority = priority;
+	}
+
+	public String getProject() {
+		return project;
+	}
+
+	public void setProject(String project) {
+		this.project = project;
+	}
+
+	public String getRecur() {
+		return recur;
+	}
+
+	public void setRecur(String recur) {
+		this.recur = recur;
+	}
+
+	public String getScheduled() {
+		return scheduled;
+	}
+
+	public void setScheduled(String scheduled) {
+		this.scheduled = scheduled;
+	}
+
+	public String getStart() {
+		return start;
+	}
+
+	public void setStart(String start) {
+		this.start = start;
+	}
+
+	public String getStatus() {
+		return status;
+	}
+
+	public void setStatus(String status) {
+		this.status = status;
+	}
+
+	public String getTags() {
+		return tags;
+	}
+
+	public void setTags(String tags) {
+		this.tags = tags;
+	}
+
+	public double getTracnumber() {
+		return tracnumber;
+	}
+
+	public void setTracnumber(double tracnumber) {
+		this.tracnumber = tracnumber;
+	}
+
+	public String getTracsummary() {
+		return tracsummary;
+	}
+
+	public void setTracsummary(String tracsummary) {
+		this.tracsummary = tracsummary;
+	}
+
+	public String getTracurl() {
+		return tracurl;
+	}
+
+	public void setTracurl(String tracurl) {
+		this.tracurl = tracurl;
+	}
+
+	public String getUntil() {
+		return until;
+	}
+
+	public void setUntil(String until) {
+		this.until = until;
+	}
+
+	public double getUrgency() {
+		return urgency;
+	}
+
+	public void setUrgency(double urgency) {
+		this.urgency = urgency;
+	}
+
+	public String getUuid() {
+		return uuid;
+	}
+
+	public void setUuid(String uuid) {
+		this.uuid = uuid;
+	}
+
+	public String getWait() {
+		return wait;
+	}
+
+	public void setWait(String wait) {
+		this.wait = wait;
+	}
+
+	public int getCustomSort() {
+		return customSort;
+	}
+
+	public void setCustomSort(int customSort) {
+		this.customSort = customSort;
+	}
+
+	public ArrayList<String> getUdaNames() {
+		return udaNames;
+	}
+
+	public void setUdaNames(ArrayList<String> udaNames) {
+		this.udaNames = udaNames;
+	}
+
+	public ArrayList<String> getUdaProperties() {
+		return udaProperties;
+	}
+
+	public void setUdaProperties(ArrayList<String> udaProperties) {
+		this.udaProperties = udaProperties;
+	}
+	
+	
 }
---
+++ AutomationAndSystems/Taskwarrior/customSortServerV4/src/customSortServerV4/TaskTest.java
@@ -0,0 +1,67 @@
+package customSortServerV4;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TaskTest {
+	private Task taskOne;
+	private Task taskTwo;
+	
+	@BeforeEach
+    public void initTasks() {
+        taskOne = new Task();
+        taskTwo = new Task();
+    }
+	
+	
+	@Test
+	void testTask() {
+		fail("Not yet implemented");
+	}
+
+	
+	@Test
+	void testGetId() {
+		taskOne.setId(3);
+		assertEquals(3, taskOne.getId());
+	}
+
+	@Test
+	void testSetId() {
+		taskOne.setId(4);
+		assertEquals(4, taskOne.getId());
+	}
+
+	@Test
+	void testGetProject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetProject() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetUrgency() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetUrgency() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGetCustomSort() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testSetCustomSort() {
+		fail("Not yet implemented");
+	}
+
+}
