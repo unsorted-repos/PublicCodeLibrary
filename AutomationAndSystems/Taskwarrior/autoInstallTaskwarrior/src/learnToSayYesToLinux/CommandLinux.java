6e8ceed1dc2aca6cd9e12e2ed3a44c592c30f3ea
akke-t <akke.toeter@gmail.com>
2019-04-02 08:15:25 +0200

    Set up reading path and executing commands.

--- AutomationAndSystems/Taskwarrior/autoInstallTaskwarrior/src/learnToSayYesToLinux/CommandLinux.java
+++ AutomationAndSystems/Taskwarrior/autoInstallTaskwarrior/src/learnToSayYesToLinux/CommandLinux.java
@@ -4,12 +4,40 @@ import java.io.*;
 import java.util.ArrayList;
 
 
+/**
+ * TODO: Before compiling, set testRun to false!
+ * Copy this script to /home/<your ubuntuUsername (which is the same as ~/)
+ * So if you store this script on your Windows disc c:/copyToUbuntu you enter:
+ * cp /mnt/c/copyToUbuntu/commandLinux.jar ~/
+ * 
+ * This script executes a series of commands to install taskwarrior on WSL Ubuntu.
+ * @author a
+ *
+ */
 public class CommandLinux {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//Test create a custom UDA
-		createUDA("testSortG","tSortG","numeric");
+		boolean testRun = true;
+		String vars = "vars";
+		
+		String[] storeUserInput =askUserInput.getUserInput();
+		for (int i = 1; i <= 50; i++) {
+			System.out.println('\n');
+		}
+		
+		String unixUserName = storeUserInput[0];
+		String unixPw = storeUserInput[1];
+		String serverName = "CN=eai.ddns.net:53589";
+		
+		//get the path of this file
+		String thisPath = getJarLocation()+"/";
+		System.out.println("Path ="+thisPath);
+		
+		// create the vars file
+		createVars(serverName);
+		
+		// execute commands
+		createUDA(testRun);
 
 		System.exit(0);
 	}
@@ -22,104 +50,74 @@ public class CommandLinux {
 	 * @param label
 	 * @param type
 	 */
-	private static void createUDA(String udaName, String label,String type) {
-		char vd = (char)124; //vertical dash: |
-		char bs = (char)92; //backslash: \		
-		String[] commands = new String[4];
+	private static void createUDA(boolean testRun) {
+		//commands[1]="task config uda."+udaName+".label "+ label;
 
-		//Check if the datatype is correct for taskwarrior:
-		if (type.equals("numeric") || type.equals("string") || type.equals("date") || type.equals("duration")){
-			//Does not work:
-			//			commands[0]="printf 'y"+bs+"n' "+vd+" sudo task config uda."+udaName+".type "+type;
-			//			commands[1]="printf 'y"+bs+"n' "+vd+" sudo task config uda."+udaName+".label "+ label;			
-
-			//Does not work:
-			//			commands[0]="printf 'yes yes "+bs+"n' "+vd+" sudo task config uda."+udaName+".type "+type;
-			//			commands[1]="printf 'yes yes "+bs+"n' "+vd+" sudo task config uda."+udaName+".label "+ label;			
-
-			//Yields an infinite stream of Adding:yes | sudo task config uda.testSortA.type numeric
-			//Runs fine with the ignoreOutput boolean on, but it does not actually create the UDA
-			//			commands[0]="yes yes "+vd+" sudo task config uda."+udaName+".type "+type;
-			//			commands[1]="yes yes "+vd+" sudo task config uda."+udaName+".label "+ label;			
-
-			//Source: https://stackoverflow.com/questions/18866381/how-can-i-run-multiple-commands-in-just-one-cmd-windows-in-java
-			//			commands[0]="sudo task config uda."+udaName+".type "+type+" & yes";
-			//			commands[1]="sudo task config uda."+udaName+".label "+ label+ " & yes";
-
-
-			//			commands[0]="sudo task config uda."+udaName+".type "+type+" & sudo yes";
-			//			commands[1]="sudo task config uda."+udaName+".label "+ label+ " & sudo yes";
-
-
-			//			commands[0]="task config uda."+udaName+".type "+type+" & sudo yes";
-			//			commands[1]="task config uda."+udaName+".label "+ label+ " & sudo yes";
-
-			//			commands[0]="task config uda."+udaName+".type "+type+" & yes";
-			//			commands[1]="task config uda."+udaName+".label "+ label+ " & yes";
-
-
-			//New try: did work manually, did not work from jar:
-			//printf 'y\n' | task config uda.testSortB.type numeric
-			//			commands[0]="printf 'y"+bs+"n' "+vd+" task config uda."+udaName+".type "+type;
-			//			commands[1]="printf 'y"+bs+"n' "+vd+" task config uda."+udaName+".label "+ label;
-
-
-			//			commands[0]="task config uda."+udaName+".type "+type;
-			//			commands[1]="yes";
-			//			commands[2]="task config uda."+udaName+".label "+ label;
-			//			commands[3]="yes";
-
-
-			//			commands[0]="task config uda."+udaName+".type "+type;
-			//			commands[1]="yes";
-			//			commands[2]="task config uda."+udaName+".label "+ label;
-			//			commands[3]="yes";
-
-			//Infinite loop at false
-			//			commands[0]="yes "+vd+" sudo task config uda."+udaName+".type "+type;
-			//			commands[1]="yes "+vd+" sudo task config uda."+udaName+".label "+ label;
-
-
-			//Did not work:
-			//			commands[0]="yes "+vd+"task config uda."+udaName+".type "+type;
-			//			commands[1]="yes";
-			//			commands[2]="yes "+vd+"task config uda."+udaName+".label "+ label;
-			//			commands[3]="yes";
-
-
-			//Did not work:
-			//			commands[0]="yes "+vd+" task config uda."+udaName+".type "+type;
-			//			commands[1]="yes "+vd+" task config uda."+udaName+".label "+ label;
-
-			//In combination with Attempt 1:
-			commands[0]="task config uda."+udaName+".type "+type;
-			commands[1]="task config uda."+udaName+".label "+ label;
-
-			runCommands(commands[0],true);
-			runCommands(commands[1], true);
-			//runCommands(commands[2],true);
-			//runCommands(commands[3], true);
-
-			//runCommands(commands[0],false);
-			//runCommands(commands[1], false);
-			//runCommands(commands[2],false);
-			//runCommands(commands[3], false);
-
-			System.out.println("Ran:"+commands[0]);
-			System.out.println("Ran:"+commands[1]);
-			System.out.println("Ran:"+commands[2]);
-			System.out.println("Ran:"+commands[3]);
-			//Trow exception if the datatype is not correct.
-		}else {
-			try {
-				throw new Exception();
-			} catch (Exception e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+		// get file path of this script
+		
+		
+		//get commands
+		String[] commands = generateCommands(testRun);
+		
+		System.out.print("Command length="+commands.length);
+		
+		// run commands
+		if (!testRun) {
+			for (int i = 0; i == commands.length; i++) {
+				runCommands(commands[i],true);
+				System.out.println("Ran:"+commands[i]);
 			}
+			
 		}
+		//runCommands(commands[1], true);
+
+		
+//		System.out.println("Ran:"+commands[1]);
+//		System.out.println("Ran:"+commands[2]);
+//		System.out.println("Ran:"+commands[3]);
+		
+		
+		// Throw exception example
+//		try {
+//			throw new Exception();
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
 	}
 
+	private static String[] generateCommands(boolean testRun) {
+		String[] commands = new String[25];
+		char vd = (char)124; //vertical dash: |
+		char bs = (char)92; //backslash: \		
+		char and = (char)38; // and $
+		char quotation = (char)34; // quotation mark "
+		
+		//In combination with Attempt 1:
+		if (!testRun) {
+			commands[0] = "yes | sudo apt update "+and+and+" sudo apt upgrade";
+			commands[1] = "sudo apt install task";
+			commands[2] = "sudo apt install taskd";
+			commands[3] = "cd ..";
+			commands[4] = "cd ..";
+			commands[5] = "export TASKDDATA=/var/taskd";
+			commands[6] = "sudo mkdir -p $TASKDDATA";
+			commands[7] = "sudo taskd init --data $TASKDDATA";
+		}else {
+				System.out.println("Skipped some lines for testing");
+		}
+		commands[8] = "cd /usr/share/taskd/pki";
+		commands[9] = "sudo nano vars";
+		commands[10] = "";
+		commands[11] = "";
+		commands[12] = "";
+		commands[13] = "";
+		commands[14] = "";
+		commands[15] = "";
+		commands[16] = "";
+		
+		return commands;
+	}
+	
 
 	public static ArrayList<ArrayList<String>> runCommands(String command,boolean ignoreOutput) {
 
@@ -129,7 +127,7 @@ public class CommandLinux {
 		ArrayList<String> errorExecutionOutput=new ArrayList<String>();
 		ArrayList<ArrayList<String>> returnLists = new ArrayList<ArrayList<String>>();
 
-		//Attempt 2:
+		// execute the command
 		Process p;
 		try {
 			p = Runtime.getRuntime().exec(command);
@@ -137,6 +135,7 @@ public class CommandLinux {
 			new Thread(new SyncPipe(p.getInputStream(), System.out)).start();
 			PrintWriter stdin = new PrintWriter(p.getOutputStream());
 			stdin.println("yes");
+			
 			// write any other commands you want here
 			stdin.close();
 			int returnCode = p.waitFor();
@@ -150,7 +149,6 @@ public class CommandLinux {
 		//Attempt 1: did not work
 		try {
 			// Execute command
-			//String command = "cmd /c start cmd.exe";
 			Process child = Runtime.getRuntime().exec(command);
 
 			// Get output stream to write from it
@@ -164,75 +162,47 @@ public class CommandLinux {
 			System.out.println("Errors are:"+e);
 		}
 
-
-		//Attempt 0:
-//		try {
-//			// run the Unix "task nice0" command
-//			Process p = Runtime.getRuntime().exec(command);
-//			BufferedReader brGood = new BufferedReader(new InputStreamReader(p.getInputStream()));
-//			BufferedReader brError = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-//
-//			// get output
-//			if (!ignoreOutput) {
-//				while ((s = brGood.readLine()) != null) {
-//					System.out.println("good output:"+s);
-//					goodExecutionOutput.add(s);
-//				}
-//
-//				// get the error message
-//				while ((s = brError.readLine()) != null) {
-//					System.out.println("Error output:"+s);
-//					errorExecutionOutput.add(s);
-//				}	
-//			}
-//
-//		}
-//		catch (IOException e) {
-//			System.out.println("Error: ");
-//			e.printStackTrace();
-//			System.exit(-1);
-//		}
-
 		//Merge outputLists and return
 		returnLists.add(goodExecutionOutput);
 		returnLists.add(errorExecutionOutput);
 		return returnLists;
-	}	
+	}
+	
+	private static void createVars(String serverName) {
+		char quotation = (char)34; // quotation mark "
+		PrintWriter writer;
+		try {
+			writer = new PrintWriter("vars", "UTF-8");
+			writer.println("BITS=4096");
+			writer.println("EXPIRATION_DAYS=365");
+			writer.println("ORGANIZATION="+quotation+"Göteborg Bit Factory"+quotation);
+			writer.println(serverName);
+			writer.println("COUNTRY=SE");
+			writer.println("STATE=\"+quotation+\"Västra Götaland"+quotation);
+			writer.println("LOCALITY="+quotation+"Göteborg"+quotation);
+			writer.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UnsupportedEncodingException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * Source:https://stackoverflow.com/questions/15359702/get-location-of-jar-file#15359999
+	 * Warning, appearently does not work in certain scenarios, the updated solution did not work in my scenario;
+	 * 		calling the .jar file from a different folder in cmd (e.g. java -jar test\myjar.jar) did not work.
+	 * @return actual location of the compiled runnable .jar file that is executed is returned from this method
+	 */
+	public static String getJarLocation() {
+		File f = new File(System.getProperty("java.class.path"));
+		File dir = f.getAbsoluteFile().getParentFile();
+		String path = dir.toString();
+		return path;
+	}
 }
-
-
-//Verify that the commands do actually work when you enter them manually in WSL Ubuntu 16.04:
-//printf 'y\n' | sudo task config uda.newTestSort.type numeric
-//printf 'y\n' | sudo task config uda.newTestSort.label nTSort
-
-//printf 'y\n' | task config uda.testSortA.type numeric
-//printf 'y\n' | task config uda.testSortA.label tSortA
-
-
-//printf 'y\n' | task config uda.testSortB.type numeric
-//printf 'y\n' | task config uda.testSortB.label tSortB
-
-//printf 'y\n' | task config uda.testSortC.type numeric
-//printf 'y\n' | task config uda.testSortC.label tSortC
-
-//task config uda.testSortD.type numeric
-//task config uda.testSortD.label tSortD
-
-//task config uda.testSortF.type numeric | yes
-//task config uda.testSortF.label tSortF | yes
-//sudo task 2 modify testSortF:24
-
-
-//yes | task config uda.testSortF.type numeric
-//yes | task config uda.testSortF.label tSortF
-//sudo task 2 modify testSortF:24
-
-//sudo task 2 modify newTestSort:29
-//inspect task 2 whether the modify command changed the description or the new uda newTestSort:
-//sudo task 2
-//Indeed Lists a new UDA named nTSort with value 29.
-
-
 
 class SyncPipe implements Runnable
 {
@@ -257,3 +227,4 @@ public SyncPipe(InputStream istrm, OutputStream ostrm) {
   private final OutputStream ostrm_;
   private final InputStream istrm_;
 }
+
---
+++ AutomationAndSystems/Taskwarrior/autoInstallTaskwarrior/src/learnToSayYesToLinux/askUserInput.java
@@ -0,0 +1,17 @@
+package learnToSayYesToLinux;
+import java.util.Scanner;
+
+public class askUserInput {
+	public static String[] getUserInput() { 
+		String[] userInput = new String[5];
+		Scanner reader = new Scanner(System.in);  // Reading from System.in
+		System.out.println("Please re-enter your Unix username: ");
+		userInput[0] = reader.next(); // Scans the next token of the input as an int.
+		System.out.println("Please re-enter your Unix password: ");
+		userInput[1] = reader.next(); // Scans the next token of the input as an int.
+		
+		//once finished
+		reader.close();
+		return userInput;
+	}
+}
