4f88afd3afc6da3f105cf746dd8b2d4f4527c982
akke-t <akke.toeter@gmail.com>
2019-05-17 18:36:23 +0200

    Tried and listed adb installation commands.

--- AutomationAndSystems/Taskwarrior/autoInstallTaskwarriorV2/src/autoInstallTaskwarrior/ExtraFeatures.java
+++ AutomationAndSystems/Taskwarrior/autoInstallTaskwarriorV2/src/autoInstallTaskwarrior/ExtraFeatures.java
@@ -33,5 +33,28 @@ public class ExtraFeatures {
 	
 	
 	// export installation data to log (without certificate key generation, without ansi-random art).
+	
+	// auto install adb android
+	/**
+	 * source: http://sonntam.github.io/build-adb/
+	 * cd ~
+	 * mkdir android
+	 * cd android
+	 * sudo apt-get install git build-essential libncurses5-dev
+	 * git clone https://android.googlesource.com/platform/system/core.git system/core
+	 * git clone https://android.googlesource.com/platform/build.git
+	 * git clone https://android.googlesource.com/platform/external/zlib.git external/zlib
+	 * git clone https://android.googlesource.com/platform/bionic.git
+	 * git clone https://android.googlesource.com/platform/external/stlport.git external/stlport
+	 * git clone https://android.googlesource.com/platform/external/libcxx.git external/libcxx
+	 * git clone https://android.googlesource.com/platform/external/openssl.git external/openssl
+	 * 
+	 */
+	
+	/**
+	 * retry:
+	 * sudo apt-get install adb
+	 */
+	
 }
 
