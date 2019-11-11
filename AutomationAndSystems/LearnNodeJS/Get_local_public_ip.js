83f41f6151d786a32c5b1f746478f93d79e11a59
unknown <a-t-0>
2019-11-11 12:36:45 +0100

    Started learning Node.js

---
+++ AutomationAndSystems/LearnNodeJS/Get_local_public_ip.js
@@ -0,0 +1,27 @@
+'use strict';
+
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+    }
+    ++alias;
+  });
+});
+
+// en0 192.168.1.101
+// eth0 10.0.0.101
---
+++ AutomationAndSystems/LearnNodeJS/Readme.md
@@ -0,0 +1,14 @@
+Install nodejs
+Open this folder in cmd
+run JavaScript file (.js) by: node "Show Node.js"
+
+0. Source:
+https://www.w3schools.com/nodejs/nodejs_get_started.asp
+The myfirst.js Runs a node server on you r pc on port 8080, which you can visit at: http://localhost:8080
+The cmd will just be blank, pause where it is, because it is just running the host.
+
+1. source: 
+https://www.w3schools.com/nodejs/nodejs_modules.asp
+myfirstmodule.js is a module=library= class. It's function is, return the time.
+then demo_module.js is the main, that hosts the node server, and imports `myfirstmodule.js`.
+
