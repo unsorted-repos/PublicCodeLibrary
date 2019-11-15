b0365053019a93fdb8e6c04400d5b1b182b7a0e7
a-t-0 <eai1@protonmail.com>
2019-11-15 21:13:10 +0100

    Included more attempts.

--- AutomationAndSystems/LearnNodeJS/medium_tutorial/fullstack_app_with_apis/backend/server.js
+++ AutomationAndSystems/LearnNodeJS/medium_tutorial/fullstack_app_with_apis/backend/server.js
@@ -14,8 +14,8 @@ const router = express.Router();
 // const dbRoute =
 //  'mongodb://<your-db-username-here>:<your-db-password-here>@ds249583.mlab.com:49583/fullstack_app';
 const dbRoute =
-  'mongodb://localhost:27017/fullstack_app';
-  
+        'mongodb://localhost:27017/fullstack_app';
+
 
 // connects our back end code with the database
 mongoose.connect(dbRoute, { useNewUrlParser: true });
@@ -36,51 +36,51 @@ app.use(logger('dev'));
 // this is our get method
 // this method fetches all available data in our database
 router.get('/getData', (req, res) => {
-  Data.find((err, data) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true, data: data });
-  });
+    Data.find((err, data) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true, data: data });
+    });
 });
 
 // this is our update method
 // this method overwrites existing data in our database
 router.post('/updateData', (req, res) => {
-  const { id, update } = req.body;
-  Data.findByIdAndUpdate(id, update, (err) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
+    const { id, update } = req.body;
+    Data.findByIdAndUpdate(id, update, (err) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true });
+    });
 });
 
 // this is our delete method
 // this method removes existing data in our database
 router.delete('/deleteData', (req, res) => {
-  const { id } = req.body;
-  Data.findByIdAndRemove(id, (err) => {
-    if (err) return res.send(err);
-    return res.json({ success: true });
-  });
+    const { id } = req.body;
+    Data.findByIdAndRemove(id, (err) => {
+        if (err) return res.send(err);
+        return res.json({ success: true });
+    });
 });
 
 // this is our create methid
 // this method adds new data in our database
 router.post('/putData', (req, res) => {
-  let data = new Data();
-
-  const { id, message } = req.body;
-
-  if ((!id && id !== 0) || !message) {
-    return res.json({
-      success: false,
-      error: 'INVALID INPUTS',
+    let data = new Data();
+    
+    const { id, message } = req.body;
+    
+    if ((!id && id !== 0) || !message) {
+        return res.json({
+            success: false,
+            error: 'INVALID INPUTS',
+        });
+    }
+    data.message = message;
+    data.id = id;
+    data.save((err) => {
+        if (err) return res.json({ success: false, error: err });
+        return res.json({ success: true });
     });
-  }
-  data.message = message;
-  data.id = id;
-  data.save((err) => {
-    if (err) return res.json({ success: false, error: err });
-    return res.json({ success: true });
-  });
 });
 
 // append /api for our http requests
--- AutomationAndSystems/LearnNodeJS/medium_tutorial/fullstack_app_with_apis/client/src/App.js
+++ AutomationAndSystems/LearnNodeJS/medium_tutorial/fullstack_app_with_apis/client/src/App.js
@@ -3,99 +3,173 @@ import React, { Component } from 'react';
 import axios from 'axios';
 
 class App extends Component {
-  // initialize our state
-  state = {
-    data: [],
-    id: 0,
-    message: null,
-    intervalIsSet: false,
-    idToDelete: null,
-    idToUpdate: null,
-    objectToUpdate: null,
-  };
-            
-  // when component mounts, first thing it does is fetch all existing data in our db
-  // then we incorporate a polling logic so that we can easily see if our db has
-  // changed and implement those changes into our UI
-  componentDidMount() {
-    this.getDataFromDb();
-    if (!this.state.intervalIsSet) {
-      let interval = setInterval(this.getDataFromDb, 1000);
-      this.setState({ intervalIsSet: interval });
+    super() {
+    this.dt=new Date();
+    this.dt_month =this.dt.getmonth()+1;
     }
-  }
-
-  // never let a process live forever
-  // always kill a process everytime we are done using it
-  componentWillUnmount() {
-    if (this.state.intervalIsSet) {
-      clearInterval(this.state.intervalIsSet);
-      this.setState({ intervalIsSet: null });
+    // initialize our state
+    state = {
+        data: [],
+        id: 0,
+        message: null,
+        intervalIsSet: false,
+        idToDelete: null,
+        idToUpdate: null,
+        objectToUpdate: null,
+    };
+    
+    // Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+    speak() {
+        console.log(`barks.`);
+    };
+    
+    // Source: 
+    // Include function to make dependent dropdown boxes:
+    jsFunction(){
+        console.log("CHANGING")
+        var list1 = document.getElementById("selectOpt1");
+        var myselect = list1.options[list1.selectedIndex].value;
+        var list2 = document.getElementById('selectOpt2');
+        var list3 = document.getElementById('selectOpt3');
+        var i;
+        for (i = 0; i < list2.options.length; i++) {
+            list2.options[i] = null;
+        }
+        for (i = 0; i < list3.options.length; i++) {
+            list3.options[i] = null;
+        }
+        if(myselect==="1"){
+            var opt = document.createElement('option');
+            opt.value = "1";
+            opt.innerHTML = "FIRST";
+            list2.appendChild(opt);
+            opt = document.createElement('option');
+            opt.value = "1";
+            opt.innerHTML = "FIRST2";
+            list3.appendChild(opt);
+        }
+        else{
+            var opt = document.createElement('option');
+            opt.value = "2";
+            opt.innerHTML = "SECOND";
+            list2.appendChild(opt);
+            opt = document.createElement('option');
+            opt.value = "1";
+            opt.innerHTML = "SECOND2";
+            list3.appendChild(opt);
+        }
     }
-  }
-
-  // just a note, here, in the front end, we use the id key of our data object
-  // in order to identify which we want to Update or delete.
-  // for our back end, we use the object id assigned by MongoDB to modify
-  // data base entries
-
-  // our first get method that uses our backend api to
-  // fetch data from our data base
-  getDataFromDb = () => {
-    fetch('http://localhost:3001/api/getData')
-      .then((data) => data.json())
-      .then((res) => this.setState({ data: res.data }));
-  };
-
-  // our put method that uses our backend api
-  // to create new query into our data base
-  putDataToDB = (message) => {
-    let currentIds = this.state.data.map((data) => data.id);
-    let idToBeAdded = 0;
-    while (currentIds.includes(idToBeAdded)) {
-      ++idToBeAdded;
+    
+    // try returning a function instead of a string:
+    returnFunction() {
+        return this.jsFunction();
     }
-
-    axios.post('http://localhost:3001/api/putData', {
-      id: idToBeAdded,
-      message: message,
-    });
-  };
-
-  // our delete method that uses our backend api
-  // to remove existing database information
-  deleteFromDB = (idTodelete) => {
-    parseInt(idTodelete);
-    let objIdToDelete = null;
-    this.state.data.forEach((dat) => {
-      if (dat.id == idTodelete) {
-        objIdToDelete = dat._id;
-      }
-    });
-
-    axios.delete('http://localhost:3001/api/deleteData', {
-      data: {
-        id: objIdToDelete,
-      },
-    });
-  };
-
-  // our update method that uses our backend api
-  // to overwrite existing data base information
-  updateDB = (idToUpdate, updateToApply) => {
-    let objIdToUpdate = null;
-    parseInt(idToUpdate);
-    this.state.data.forEach((dat) => {
-      if (dat.id == idToUpdate) {
-        objIdToUpdate = dat._id;
-      }
-    });
-
-    axios.post('http://localhost:3001/api/updateData', {
-      id: objIdToUpdate,
-      update: { message: updateToApply },
-    });
-  };
+    
+    // Source: https://www.plus2net.com/javascript_tutorial/list-adding.php
+    //this.dt=new Date();
+    //this.dt_month=dt.getMonth() +1;
+    
+   
+    addOption(selectbox,text,value )
+    {
+        var optn = document.createElement("OPTION");
+        optn.text = text;
+        optn.value = value;
+        selectbox.options.add(optn);
+    }
+    
+    addOption_list(){
+        var month = new Array("January","February","March","April","May","June","July","August",
+        "September","October","November","December");
+        for (var i=0; i < month.length;++i){
+            this.addOption(document.drop_list.Month_list, month[i], month[i]);
+            if(i== this.dt_month){document.drop_list.Month_list.options[i].selected=true;}
+        }
+    }
+    
+    
+    // when component mounts, first thing it does is fetch all existing data in our db
+    // then we incorporate a polling logic so that we can easily see if our db has
+    // changed and implement those changes into our UI
+    componentDidMount() {
+        this.getDataFromDb();
+        if (!this.state.intervalIsSet) {
+            let interval = setInterval(this.getDataFromDb, 10009);
+            this.setState({ intervalIsSet: interval });
+        }
+    }
+    
+    // never let a process live forever
+    // always kill a process everytime we are done using it
+    componentWillUnmount() {
+        if (this.state.intervalIsSet) {
+            clearInterval(this.state.intervalIsSet);
+            this.setState({ intervalIsSet: null });
+        }
+    }
+    
+    // just a note, here, in the front end, we use the id key of our data object
+    // in order to identify which we want to Update or delete.
+    // for our back end, we use the object id assigned by MongoDB to modify
+    // data base entries
+    
+    // our first get method that uses our backend api to
+    // fetch data from our data base
+    getDataFromDb = () => {
+        fetch('http://localhost:3001/api/getData')
+                .then((data) => data.json())
+                .then((res) => this.setState({ data: res.data }));
+    };
+    
+    // our put method that uses our backend api
+    // to create new query into our data base
+    putDataToDB = (message) => {
+        let currentIds = this.state.data.map((data) => data.id);
+        let idToBeAdded = 0;
+        while (currentIds.includes(idToBeAdded)) {
+            ++idToBeAdded;
+        }
+        
+        axios.post('http://localhost:3001/api/putData', {
+            id: idToBeAdded,
+            message: message,
+        });
+    };
+    
+    // our delete method that uses our backend api
+    // to remove existing database information
+    deleteFromDB = (idTodelete) => {
+        parseInt(idTodelete);
+        let objIdToDelete = null;
+        this.state.data.forEach((dat) => {
+            if (dat.id == idTodelete) {
+                objIdToDelete = dat._id;
+            }
+        });
+        
+        axios.delete('http://localhost:3001/api/deleteData', {
+            data: {
+                id: objIdToDelete,
+            },
+        });
+    };
+    
+    // our update method that uses our backend api
+    // to overwrite existing data base information
+    updateDB = (idToUpdate, updateToApply) => {
+        let objIdToUpdate = null;
+        parseInt(idToUpdate);
+        this.state.data.forEach((dat) => {
+            if (dat.id == idToUpdate) {
+                objIdToUpdate = dat._id;
+            }
+        });
+        
+        axios.post('http://localhost:3001/api/updateData', {
+            id: objIdToUpdate,
+            update: { message: updateToApply },
+        });
+    };
 
   // here is our UI
   // it is easy to understand their functions when you
@@ -114,8 +188,8 @@ class App extends Component {
                   {dat.message}
                 </li>
               ))}
-        </ul>
-        <div style={{ padding: '10px' }}>
+        </ul> 
+       <div style={{ padding: '10px' }}>
           <input
             type="text"
             onChange={(e) => this.setState({ message: e.target.value })}
@@ -159,104 +233,142 @@ class App extends Component {
           </button>
         </div>
 		
-        // include dropdown: 1
-		
-        <select>
-            <option value="volvo">Volvo</option>
-            <option value="saab">Saab</option>
-            <option value="mercedes">Mercedes</option>
-            <option value="audi">Audi</option>
-        </select>
-
-        // include dropdown: 2 Fails:
+        <br></br>
+        <br></br>
+        // include dropdown: 1<br></br>
+        <div>	
+            <select>
+                <option value="volvo">Volvo</option>
+                <option value="saab">Saab</option>
+                <option value="mercedes">Mercedes</option>
+                <option value="audi">Audi</option>
+            </select>
+        </div>
+        
+        <br></br>
+        <br></br>
+        // include dropdown: 2 Fails:<br></br>
+        
 	
-        // include dropdown: 3 Fails:
-        // Source: https://stackoverflow.com/questions/26218243/dynamic-dropdown-in-node-js
-	<select id="skill_category">
-            <option value="communication">Communication</option>
-            <option value="teamwork">Team Work</option>
-            <option value="technical">Technical</option>
-	</select> 
+        <br></br>
+        <br></br>
+        // include dropdown: 3 Fails:<br></br>
+        // Source: https://stackoverflow.com/questions/26218243/dynamic-dropdown-in-node-js<br></br>
+        <div>
+            <select id="skill_category">
+                <option value="communication">Communication</option>
+                <option value="teamwork">Team Work</option>
+                <option value="technical">Technical</option>
+            </select> 
 
-	<select class="skill" id="communciation">
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-	</select> 
-	<select  class="skill" id="teamwork">
-            <option value="a">One</option>
-            <option value="b">Two</option>
-            <option value="c">Three</option>
-	</select> 
-	<select  class="skill" id="technical">
-            <option value="1">One</option>
-            <option value="2">Two</option>
-            <option value="3">Three</option>
-	</select> 
+            <select class="skill" id="communciation">
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select> 
+            <select  class="skill" id="teamwork">
+                <option value="a">One</option>
+                <option value="b">Two</option>
+                <option value="c">Three</option>
+            </select> 
+            <select  class="skill" id="technical">
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </select> 
+        </div>
         
-        // include dropdown 4:
-        // Source: https://stackoverflow.com/questions/29623225/javascript-dependent-drop-down-list
-        // Source: https://jsfiddle.net/v917ycp6/5/
-        <form id="formname" name="formname" method="post" action="submitform.asp" >
-            <table width="50%" border="0" cellspacing="0" cellpadding="5">
-                <tr>
-                    <td width="41%" align="right" valign="middle">Category1 :</td>
-                    <td width="59%" align="left" valign="middle">
-                        <select name="category1" id="category1">
-                            <option value="">Select Category1</option>
-                            <option value="home_ware">Home Ware</option>
-                            <option value="education">Education</option>
-                            <option value="books">Books</option>
-                        </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td align="right" valign="middle">Category2 :</td>
-                    <td align="left" valign="middle">
-                        <select disabled="disabled" id="category2" name="category2">
-                            <option value>Select Category2</option>
+        <br></br>
+        <br></br>
+        // include dropdown 4:<br></br>
+        // Source: https://stackoverflow.com/questions/29623225/javascript-dependent-drop-down-list<br></br>
+        // Source: https://jsfiddle.net/v917ycp6/5/<br></br>
+        <div>
+            <form id="formname" name="formname" method="post" action="submitform.asp" >
+                <table width="50%" border="0" cellspacing="0" cellpadding="5">
+                    <tr>
+                        <td width="41%" align="right" valign="middle">Category1 :</td>
+                        <td width="59%" align="left" valign="middle">
+                            <select name="category1" id="category1">
+                                <option value="">Select Category1</option>
+                                <option value="home_ware">Home Ware</option>
+                                <option value="education">Education</option>
+                                <option value="books">Books</option>
+                            </select>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td align="right" valign="middle">Category2 :</td>
+                        <td align="left" valign="middle">
+                            <select disabled="disabled" id="category2" name="category2">
+                                <option value>Select Category2</option>
 
-                            <option rel="home_ware" value="air-conditioners_coolers">Air-Conditioners/Coolers</option>
-                            <option rel="home_ware" value="audio-video">Audio/Video</option>
-                            <option rel="home_ware" value="beddings">Beddings</option>
-                            <option rel="home_ware" value="camera">Camera</option>
-                            <option rel="home_ware" value="cell-phones">Cell Phones</option>
+                                <option rel="home_ware" value="air-conditioners_coolers">Air-Conditioners/Coolers</option>
+                                <option rel="home_ware" value="audio-video">Audio/Video</option>
+                                <option rel="home_ware" value="beddings">Beddings</option>
+                                <option rel="home_ware" value="camera">Camera</option>
+                                <option rel="home_ware" value="cell-phones">Cell Phones</option>
 
-                            <option rel="Education" value="Colleges">Colleges</option>
-                            <option rel="Education" value="Institutes">Institutes</option>
-                            <option rel="Education" value="Schools">Schools</option>
-                            <option rel="Education" value="Tuitions">Tuitions</option>
-                            <option rel="Education" value="Universities">Universities</option>
+                                <option rel="Education" value="Colleges">Colleges</option>
+                                <option rel="Education" value="Institutes">Institutes</option>
+                                <option rel="Education" value="Schools">Schools</option>
+                                <option rel="Education" value="Tuitions">Tuitions</option>
+                                <option rel="Education" value="Universities">Universities</option>
 
-                            <option rel="Books" value="College Books">College Books</option>
-                            <option rel="Books" value="Engineering">Engineering</option>
-                            <option rel="Books" value="Magazines">Magazines</option>
-                            <option rel="Books" value="Medicine">Medicine</option>
-                            <option rel="Books" value="References">References</option>
-                        </select>
-                    </td>
-                </tr>
-            </table>
-	</form>
+                                <option rel="Books" value="College Books">College Books</option>
+                                <option rel="Books" value="Engineering">Engineering</option>
+                                <option rel="Books" value="Magazines">Magazines</option>
+                                <option rel="Books" value="Medicine">Medicine</option>
+                                <option rel="Books" value="References">References</option>
+                            </select>
+                        </td>
+                    </tr>
+                </table>
+            </form>
+        </div>
         
-        
+        <br></br>
+        <br></br>
         // include dropdown 5:
-         <select onChange="jsFunction()" id="selectOpt1">
-          <option value="1" selected="selected">1</option>
-          <option value="2">2</option>
-        </select>
-        <select id="selectOpt2">
-          <option value="1">FIRST</option>
-        </select>
-        <select id="selectOpt3">
-          <option value="1">FIRST</option>
-        </select>
-        
+        <div>
+            <select onChange="this.returnFunction" id="selectOpt1">
+                <option value="1" selected="selected">1</option>
+                <option value="2">2</option>
+            </select>
+            <select id="selectOpt2">
+                 <option value="1">FIRST</option>
+            </select>
+            <select id="selectOpt3">
+                <option value="1">FIRST</option>
+            </select>
+        </div>
+                
+        <br></br>
+        <br></br>
+        // include dropdown 6:
+        <div>
+            <button onClick={() => this.addOption_list()}>
+            ADD
+          </button>
+        </div>
             
-        
+        // include dropdown 7:
+        <div onClick="this.addOption_list()">
+            You can see the view-> Source of this page. 
+            <br></br>
+            <form name="drop_list" action="yourpage.php" method="POST" >
+                <select NAME="Month_list">
+                    <option value="" >Month list</option>
+                </select>
+            </form>
+        </div>
     </div>
     );
   }
 }
+
+// Source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Classes
+//let d = new App('Mitzie');
+//d.speak();
 
 export default App;
