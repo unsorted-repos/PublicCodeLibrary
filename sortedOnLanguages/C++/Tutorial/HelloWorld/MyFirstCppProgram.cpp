f1e0790579e3968915334366e225eaef9e820351
a-t-0 <eai1@protonmail.com>
2020-05-03 13:20:43 +0200

    Created helloworld in c++.

---
+++ sortedOnLanguages/C++/Tutorial/HelloWorld/MyFirstCppProgram.cpp
@@ -0,0 +1,20 @@
+/*
+ * MyFirstCppProgram.cpp
+ *
+ *  Created on: May 3, 2020
+ *      Author: a
+ */
+
+# include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Hello c++ world" << endl;
+}
+
+/*
+ * Source: https://www.youtube.com/watch?v=D2oAUDPWkkA
+ * 0. Clean previously compiled files with: Project>Clean
+ * 1. Press: Compile = hammer
+ * 2. Then press triangle next to run>Local C/C++ Application
+ */
