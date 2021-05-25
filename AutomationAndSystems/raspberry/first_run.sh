aec7186884680986db1f077731812d04698e623f
a-t-0 <eai@protonmail.com>
2021-05-25 07:14:09 +0200

    Corrected typo.

--- AutomationAndSystems/raspberry/first_run.sh
+++ AutomationAndSystems/raspberry/first_run.sh
@@ -19,26 +19,26 @@ do
 	# get wifi configuration file
 	config_filename=$(ls /etc/netplan)
 	echo $config_filename
-	config_filepath="/etc/netplan/$$config_filename"
+	config_filepath="/etc/netplan/$config_filename"
 	
 	# check if the wifi is already added.
 	if ! grep -q "$wlan_module" "$config_filepath"; then
 		echo "have to add wifi"
 
 		# ask  wifi pwd ssid
-		read -p "What is the wifi ssid you want to connect to?" ssid
+		read -p "What is the wifi ssid you want to connect to(Case sensitive)?" ssid
 
 		# ask wifi pwd
 		read -p "What is the wifi pwd?" pwd
 
 		# append content
-		sudo echo "    wifis:" >> $config_filepath 
-		sudo echo "        wlan0:" >> $config_filepath
-		sudo echo "            dhcp4: true" >> $config_filepath
-		sudo echo "            optional: true" >> $config_filepath
-		sudo echo "            access-points:" >> $config_filepath
-		sudo echo "                \"$ssid\":" >> $config_filepath
-		sudo echo "                    password: \"$pwd\"" >> $config_filepath
+		echo "    wifis:" | sudo tee -a $config_filepath 
+		echo "        wlan0:" | sudo tee -a $config_filepath
+		echo "            dhcp4: true" | sudo tee -a $config_filepath
+		echo "            optional: true" | sudo tee -a $config_filepath
+		echo "            access-points:" | sudo tee -a $config_filepath
+		echo "                \"$ssid\":" | sudo tee -a $config_filepath
+		echo "                    password: \"$pwd\"" | sudo tee -a $config_filepath
 
 		# generate a config file
 		sudo netplan generate
@@ -54,12 +54,12 @@ cd ~
 git clone https://github.com/HiveMinds-EU/Productivity-setup.git
 
 # update and upgrade
-sudo apt update
-sudo apt upgrade
+yes | sudo apt update
+yes | sudo apt upgrade
 
 # update and upgrade twice to allow for delays
-sudo apt update
-sudo apt upgrade
+yes | sudo apt update
+yes | sudo apt upgrade
 
 # install conda
 wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-armv7l.sh -O ~/miniconda.sh
